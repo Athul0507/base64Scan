cb538d336afae008078e2e02c3d2e00fb2a62392
Athul I Kumar <athulikumar0507@gmail.com>
2024-03-21 21:55:58 +0530

    post

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res)=>{
     res.json({"text": "hi how are you"})
 })
 
-app.get('/extractText', async (req, res) => {
+app.post('/extractText', async (req, res) => {
     try {
         const base64Image = req.body.image; // Assuming the base64 image is sent as 'image' in the request body
         console.log(base64Image)
